d47ae274179eedcb04c320f102ef0cb4cc1055e1
TereshinaEVa <tereshina.eva@gmail.com>
2024-05-19 13:17:10 +0300

    Списки и словари

---
+++ Homework5.py
@@ -0,0 +1,14 @@
+'''my_list = ['apple', 'orange', 'grape', 'cherry', 'kivi', 'pear']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:6])
+my_list[2] = 'grapefruit'
+print(my_list)'''
+
+my_dict = {'apple':'яблоко', 'orange':'апельсин', 'grape':'виноград', 'cherry':'вишня', 'kivi':'киви', 'pear':'груша'}
+print(my_dict)
+print(my_dict['grape'])
+my_dict['orange'] = 'АпЕлЬсИн'
+print(my_dict)
+my_dict['banana'] = 'банан'
+print(my_dict)
