a2c6bfc8f6e9697a5a0073e6b7c66321cf731fad
TereshinaEVa <tereshina.eva@gmail.com>
2024-05-19 12:04:55 +0300

    Кортежи

---
+++ Homework4.py
@@ -0,0 +1,8 @@
+immutable_var = (1, 2, 3, [5, 6], True)
+print(immutable_var)
+#immutable_var[1] = 3
+#print(immutable_var)
+
+mutadle_list = ([12, 25, 45, 60])
+mutadle_list[0] = 16
+print(mutadle_list)
